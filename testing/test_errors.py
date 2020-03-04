976c0f6289750da9786ea72ce1dd5eba84fb6898
Stephen Wight <srwight@gmail.com>
2020-03-04 01:17:28 -0500

    More linting

--- testing/test_errors.py
+++ testing/test_errors.py
@@ -1,22 +1,34 @@
+'''
+Tests error endpoints
+'''
 from flask.json import loads
 
 def test_bad_request_400(test_client):
+    '''
+    Tests endpoint for 400 errors
+    '''
     response = test_client.post('/json', json={"this":"that"})
     response_json = loads(response.data.decode('utf-8'))
     assert response.status_code == 400
-    assert all([value in response_json for value in ['error','format']])
+    assert all([value in ['error', 'format'] for value in response_json])
 
 def test_unknown_404(test_client):
+    '''
+    Tests endpoint for 404 errors
+    '''
     response = test_client.get('/bad_url')
     assert response.status_code == 404
     assert b"404 Error Marker" in response.data
-    
+
     response = test_client.get('/bad_url', data='{"this":"that"}', content_type="application/json")
     response_json = loads(response.data.decode('utf-8'))
     assert response.status_code == 404
     assert 'error' in response_json
 
 def test_wrong_method_405(test_client):
+    '''
+    Tests endpoint for 405 errors
+    '''
     response = test_client.post('/', content_type="application/json")
     response_json = loads(response.data.decode('utf-8'))
     assert response.status_code == 405
@@ -27,6 +39,9 @@ def test_wrong_method_405(test_client):
     assert b"405 Error Marker" in response.data
 
 def test_teapot_418(test_client):
+    '''
+    Tests endpoint for 418 errors
+    '''
     response = test_client.post('/json', content_type="application/html")
     assert response.status_code == 418
-    assert b"418 Error Marker" in response.data    
+    assert b"418 Error Marker" in response.data
--- testing/test_sudoku_check.py
+++ testing/test_sudoku_check.py
@@ -1,15 +1,18 @@
+'''
+Checks functionality of all functions within sudoku_check.py
+'''
+from json import load
+import numpy as np
 from server.sudoku_check import check_shape_contents,\
                                 check_segment,\
                                 preprocess,\
                                 stack_squares,\
                                 sudoku_check
-from json import load
-import numpy as np
 
-pass_row = np.array([1,3,4,2,5,7,6,9,8])
-fail_row = np.array([1,1,2,5,4,6,2,3,6])
+PASS_ROW = np.array([1, 3, 4, 2, 5, 7, 6, 9, 8])
+FAIL_ROW = np.array([1, 1, 2, 5, 4, 6, 2, 3, 6])
 
-pass_data = [\
+PASS_DATA = [\
     '912468753',
     '867135492',
     '435279168',
@@ -31,7 +34,7 @@ pass_data_squares = [\
     '726891543',
     '314527986']
 
-fail_shape = [\
+FAIL_SHAPE = [\
         '912468753',
         '867135492',
         '435279168',
@@ -41,7 +44,7 @@ fail_shape = [\
         '598726314',
         '271543986']
 
-fail_shape_2 = [\
+FAIL_SHAPE_2 = [\
     '912468753',
     '867135492',
     '435279168',
@@ -52,7 +55,7 @@ fail_shape_2 = [\
     '346891527',
     '271543986']
 
-fail_contents = [\
+FAIL_CONTENTS = [\
     '912468753',
     '867135492',
     '435279168',
@@ -64,37 +67,55 @@ fail_contents = [\
     '271543986']
 
 with open('testing/testdata.json', 'rt') as fl_in:
-    fail_data = load(fl_in)
+    FAIL_DATA = load(fl_in)
 
 def test_check_shape_contents():
-    for errortype in fail_data:
-        for test in fail_data[errortype]:
-            assert check_shape_contents(fail_data[errortype][test])
-    
-    assert not check_shape_contents(fail_shape)
-    assert not check_shape_contents(fail_shape_2)
-    assert not check_shape_contents(fail_contents)
+    '''
+    Tests check_shape_contents by passing one too few rows,
+    one too few items in a row, and invalide cell contents
+    '''
+    for errortype in FAIL_DATA:
+        for test in FAIL_DATA[errortype]:
+            assert check_shape_contents(FAIL_DATA[errortype][test])
+
+    assert not check_shape_contents(FAIL_SHAPE)
+    assert not check_shape_contents(FAIL_SHAPE_2)
+    assert not check_shape_contents(FAIL_CONTENTS)
 
 def test_preprocess():
-    result=preprocess(pass_data)
+    '''
+    Tests to be sure that preprocess is returning a numpy array
+    of shape (9, 9)
+    '''
+    result = preprocess(PASS_DATA)
     assert type(result).__module__ == np.__name__
-    assert result.shape == (9,9)
+    assert result.shape == (9, 9)
 
 def test_stack_squares():
-    result = preprocess(pass_data)
+    '''
+    Tests to see if the squares are being unfolded properly by
+    comparing a known board to a manually unfolded board
+    '''
+    result = preprocess(PASS_DATA)
     result_squares = preprocess(pass_data_squares)
-    result=stack_squares(result)
+    result = stack_squares(result)
     np.testing.assert_array_equal(result, result_squares)
 
 def test_check_segment():
-    assert check_segment(pass_row)
-    assert not check_segment(fail_row)
+    '''
+    Tests the check_segment function by passing it good rows and bad rows
+    '''
+    assert check_segment(PASS_ROW)
+    assert not check_segment(FAIL_ROW)
 
 def test_sudoku_check():
-    for errortype in fail_data:
-        for test in fail_data[errortype]:
-            assert not sudoku_check(fail_data[errortype][test])
+    '''
+    tests the concatenating function
+    '''
+    for errortype in FAIL_DATA:
+        for test in FAIL_DATA[errortype]:
+            assert not sudoku_check(FAIL_DATA[errortype][test])
 
-    assert not sudoku_check(fail_contents)
-    
-    assert sudoku_check(pass_data)
+    assert not sudoku_check(FAIL_CONTENTS)
+
+    assert sudoku_check(PASS_DATA)
